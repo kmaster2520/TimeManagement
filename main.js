fda8b89a953ef9fb3d2a2d9ce2dcaa1334e508f6
Sathvik Kadaveru <Sathvik@lawn-143-215-122-117.lawn.gatech.edu>
2015-10-17 22:42:30 -0400

    second commit

--- main.js
+++ main.js
@@ -5,12 +5,16 @@ $(document).ready(function() {
                   var rowHeight = 100
                   var tileSize = 16
                   
+                  context.rect(0, 0, 720, 500);
+                  context.fillStyle='#FFFFFF';
+                  context.fill();
+                  
                   for (var i = 0; i < 144; i++) {
                   
                   var r = parseInt(i / 36);
                   var c = i % 36;
                   
-                  context.rect(c * tileSize + 50, r * rowHeight + 100, tileSize, tileSize);
+                  context.rect(c * tileSize + 65, r * rowHeight + 100, tileSize, tileSize);
                   context.strokeStyle = '#000000';
                   context.stroke();
                   
@@ -18,8 +22,26 @@ $(document).ready(function() {
                   }
                   
                   context.font = "16px Arial";
-                  context.fillText("12 AM", 0, 115);
-                  context.fillText("6 AM",  0, 115 + rowHeight);
-                  context.fillText("12 PM", 0, 115 + rowHeight * 2);
-                  context.fillText("6 PM",  0, 115 + rowHeight * 3);
+                  context.fillStyle='#000000';
+                  context.fillText("12 AM", 10, 115);
+                  context.fillText("06 AM", 10, 115 + rowHeight);
+                  context.fillText("12 PM", 10, 115 + rowHeight * 2);
+                  context.fillText("06 PM", 10, 115 + rowHeight * 3);
+                  
+                  
+                  presentData()
+                  
+                  
+                  
 });
+
+function getData() {
+    
+}
+
+function presentData() {
+    var dataPoints = getData()//list of tuples
+    for (var i = 0; dataPoints.length; i++) {
+        
+    }
+}
