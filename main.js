d07bde508513f3e551966b3753387958abc36092
Sathvik Kadaveru <Sathvik@lawn-143-215-122-117.lawn.gatech.edu>
2015-10-18 00:52:59 -0400

    Labels Added

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 var canvas, context;
 var rowHeight, tileSize;
+var tlc = ['#FF00FF', '#00FFFF'];
+
+function sortByStartTime(arr) {
+    for (var i = 1; i < arr.length; i++) {
+        for (var j = i - 1; j >= 0; j--) {
+            if (arr[j + 1] < arr[j]) {
+                var temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            } else {
+                break;
+            }
+        }
+    }
+    return arr;
+}
 
 function getData() {
     var reddit = Array();
@@ -9,7 +25,7 @@ function getData() {
 }
 
 function presentData() {
-    var dataPoints = getData()//list of tuples
+    var dataPoints = sortByStartTime(getData())//list of tuples
     for (var i = 0; i < dataPoints.length; i++) {
         console.log(dataPoints[i]);
         var name = dataPoints[i][0];
@@ -25,30 +41,29 @@ function presentData() {
             context.rect(c * tileSize + 65, r * rowHeight + 100, tileSize, tileSize);
             context.strokeStyle = '#000000';
             context.stroke();
-            context.fillStyle = '#FF0000';
+            context.fillStyle = tlc[i % 2];
             context.fill();
         }
         
-        context.rotate(-Math.PI/2);
-        context.font = "16px Arial";
+        var r = parseInt(start / 36);
+        var c = start % 36;
+        context.font = "14px Arial";
         context.fillStyle='#000000';
-        context.fillText("12 AM", 10, 115);
-        
-        context.restore();
+        context.fillText(name, c * tileSize + 66, r * rowHeight + 98 , 115);
     }
 }
 
-function init() {
-    canvas = document.getElementById("bar");
-    context = canvas.getContext('2d');
+function redraw() {
     
-    rowHeight = 100
-    tileSize = 16
-    
+}
+
+function fillColor(color) {
     context.rect(0, 0, 720, 500);
-    context.fillStyle='#FFFFFF';
+    context.fillStyle = color;
     context.fill();
-    
+}
+
+function drawTimeLine() {
     for (var i = 0; i < 144; i++) {
         
         var r = parseInt(i / 36);
@@ -58,16 +73,24 @@ function init() {
         context.rect(c * tileSize + 65, r * rowHeight + 100, tileSize, tileSize);
         context.strokeStyle = '#222222';
         context.stroke();
-        
-        
     }
-    
     context.font = "16px Arial";
-    context.fillStyle='#000000';
+    context.fillStyle = '#000000';
     context.fillText("12 AM", 10, 115);
     context.fillText("06 AM", 10, 115 + rowHeight);
     context.fillText("12 PM", 10, 115 + rowHeight * 2);
     context.fillText("06 PM", 10, 115 + rowHeight * 3);
+}
+
+function init() {
+    canvas = document.getElementById("bar");
+    context = canvas.getContext('2d');
+    
+    rowHeight = 100
+    tileSize = 16
+    
+    fillColor('#FFFFFF');
+    drawTimeLine();
     
     presentData();
 }
